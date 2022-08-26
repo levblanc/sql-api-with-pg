75539e219df684c52abbb3013b415d96b3784b83
levblanc <levblanc@gmail.com>
2022-08-26 18:56:02 +0800

    Wire up find user by ID route

--- src/repos/user-repo.js
+++ src/repos/user-repo.js
@@ -8,7 +8,14 @@ class UserRepo {
     return toCamelCase(rows);
   }
 
-  static async findById() {}
+  static async findById(id) {
+    const { rows } = await pool.query(
+      'SELECT * FROM users WHERE users.id = $1;',
+      [id]
+    );
+
+    return toCamelCase(rows)[0];
+  }
 
   static async insert() {}
 
